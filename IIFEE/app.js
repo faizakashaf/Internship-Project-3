0af1dbd631dff845e1498cd620fcf924cb22a0f6
Faiza Kashaf <faizakashaf4@gmail.com>
2024-07-22 16:36:08 +0500

    done invoked functions

---
+++ IIFEE/app.js
@@ -0,0 +1,18 @@
+//Example 01
+(function(){
+    console.log("invoked")
+})()
+
+
+//Example 02
+(function(name){
+console.log(name)
+})("faiza kashaf")
+
+
+//Example 03
+const result = (function(){
+return 2+5
+})();
+
+console.log(result)
